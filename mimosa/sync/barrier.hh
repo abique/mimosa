93154ef5597410ab3a516bbbf5416309ca95b754
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-23 10:06:17 +0900

    Added barrier to mimosa

---
+++ mimosa/sync/barrier.hh
@@ -0,0 +1,35 @@
+#ifndef MIMOSA_SYNC_BARRIER_HH
+# define MIMOSA_SYNC_BARRIER_HH
+
+# include <stdexcept>
+
+# include <melon/melon.h>
+
+# include "../non-copyable.hh"
+
+namespace mimosa
+{
+  namespace sync
+  {
+    class Barrier : private NonCopyable
+    {
+    public:
+      inline Barrier(uint16_t nb) throw std::bad_alloc
+        : barrier_(::melon_barrier_new(nb))
+      {
+        if (!barrier_)
+          throw std::bad_alloc;
+      }
+
+      inline ~Barrier() { ::melon_barrier_destroy(barrier_); }
+      inline void add(uint16_t nb = 1) { ::melon_barrier_add(barrier_, nb); }
+      inline void release(uint16_t nb = 1) { ::melon_barrier_release(barrier_, nb); }
+      inline void wait() { ::melon_barrier_wait(barrier_); }
+
+    private:
+      melon_barrier * barrier_;
+    };
+  }
+}
+
+#endif /* !MIMOSA_SYNC_BARRIER_HH */
