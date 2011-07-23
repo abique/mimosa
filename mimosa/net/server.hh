3c1102b8f023d5fb87d076f6e36407fca5b69c32
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-23 18:06:32 +0900

    Added server

---
+++ mimosa/net/server.hh
@@ -0,0 +1,17 @@
+#ifndef MIMOSA_NET_SERVER_HH
+# define MIMOSA_NET_SERVER_HH
+
+namespace mimosa
+{
+  namespace net
+  {
+    class Server
+    {
+    public:
+    private:
+      int fd_;
+    };
+  }
+}
+
+#endif /* !MIMOSA_NET_SERVER_HH */
