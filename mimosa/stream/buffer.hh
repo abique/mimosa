a2a47f136bd9fb38081e95fb869261531657f8cd
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-29 02:57:13 +0900

    Added a class buffer

---
+++ mimosa/stream/buffer.hh
@@ -0,0 +1,30 @@
+#ifndef MIMOSA_STREAM_BUFFER_HH
+# define MIMOSA_STREAM_BUFFER_HH
+
+# include "../ref-countable.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    class Buffer : public RefCountable<Buffer>
+    {
+    public:
+      typedef container::IntrusiveSlist<Buffer, &Buffer::next_> Slist;
+
+      Buffer(uint64_t size = 64 * 1024);
+      ~Buffer();
+
+      inline uint64_t size() const { return size_; }
+      inline char * data() { return data_; }
+      inline const char * data() const { return data_; }
+
+    protected:
+      container::IntrusiveSlist<Buffer> next_;
+      uint64_t                          size_;
+      char *                            data_;
+    };
+  }
+}
+
+#endif /* !MIMOSA_STREAM_BUFFER_HH */
