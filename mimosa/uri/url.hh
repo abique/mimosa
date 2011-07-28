93f3bef6c23d806a476f0b4f195a0725a44b90cc
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-28 22:56:06 +0900

    Added a class url

---
+++ mimosa/uri/url.hh
@@ -0,0 +1,16 @@
+#ifndef MIMOSA_URI_URL_HH
+# define MIMOSA_URI_URL_HH
+
+# include "../ref-countable.hh"
+
+namespace mimosa
+{
+  namespace uri
+  {
+    class Url : public RefCountable<Url>
+    {
+    };
+  }
+}
+
+#endif /* !MIMOSA_URI_URL_HH */
