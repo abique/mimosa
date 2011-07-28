a16ace620410a758a82e840fa817c4c8d58b6000
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-28 23:49:40 +0900

    added typedef Time.

--- mimosa/runtime/time.hh
+++ mimosa/runtime/time.hh
@@ -7,15 +7,16 @@ namespace mimosa
 {
   namespace runtime
   {
-    inline ::melon_time_t time() { return ::melon_time(); }
-    inline ::melon_time_t nanoseconds(int64_t ns) { return ns; }
-    inline ::melon_time_t microseconds(int64_t us) { return us * 1000; }
-    inline ::melon_time_t milliseconds(int64_t ms) { return ms * 1000 * 1000; }
-    inline ::melon_time_t seconds(int64_t s) { return s * 1000 * 1000 * 1000; }
-    inline ::melon_time_t minutes(int64_t m) { return m * 60 * 1000 * 1000 * 1000; }
-    inline ::melon_time_t hours(int64_t h) { return h * 60 * 60 * 1000 * 1000 * 1000; }
-    inline ::melon_time_t days(int64_t d) { return d * 24 * 60 * 60 * 1000 * 1000 * 1000; }
-    inline void sleep(melon_time_t duration) { ::melon_usleep(duration / 1000); }
+    typedef ::melon_time_t Time;
+    inline Time time() { return ::melon_time(); }
+    inline Time nanoseconds(int64_t ns) { return ns; }
+    inline Time microseconds(int64_t us) { return us * 1000; }
+    inline Time milliseconds(int64_t ms) { return ms * 1000 * 1000; }
+    inline Time seconds(int64_t s) { return s * 1000 * 1000 * 1000; }
+    inline Time minutes(int64_t m) { return m * 60 * 1000 * 1000 * 1000; }
+    inline Time hours(int64_t h) { return h * 60 * 60 * 1000 * 1000 * 1000; }
+    inline Time days(int64_t d) { return d * 24 * 60 * 60 * 1000 * 1000 * 1000; }
+    inline void sleep(Time duration) { ::melon_usleep(duration / 1000); }
   }
 }
 
