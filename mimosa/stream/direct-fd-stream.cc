3ce2c27532c72ee84acb25226e938f4f8e749039
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-29 17:34:41 +0900

    direct-fd-stream: checking read/write ability and removed flush()

---
+++ mimosa/stream/direct-fd-stream.cc
@@ -0,0 +1,40 @@
+#include "direct-fd-stream.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    DirectFdStream::DirectFdStream(int fd, bool readable, bool writable)
+      : fd_(fd)
+    {
+    }
+
+    DirectFdStream::~DirectFdStream()
+    {
+      ::close(fd_);
+      fd_ = -1;
+    }
+
+    int64_t
+    DirectFdStream::write(const char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      if (!is_writable_)
+      {
+        errno = EINVAL;
+        return -1;
+      }
+      return ::mimosa_write(fd_, data, nbytes, timeout);
+    }
+
+    int64_t
+    DirectFdStream::read(char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      if (!is_readable_)
+      {
+        errno = EINVAL;
+        return -1;
+      }
+      return ::mimosa_read(fd_, data, nbytes, timeout);
+    }
+  }
+}
---
+++ mimosa/stream/direct-fd-stream.hh
@@ -0,0 +1,29 @@
+#ifndef MIMOSA_STREAM_DIRECT_FD_STREAM_HH
+# define MIMOSA_STREAM_DIRECT_FD_STREAM_HH
+
+# include "stream.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    class DirectFdStream : public Stream
+    {
+    public:
+      DirectFdStream(int fd, bool readable = 1, bool writable = 1, bool is_sequential = 1);
+      virtual ~DirectFdStream();
+
+      virtual int64_t write(const char * data, uint64_t nbytes, runtime::Time timeout);
+      virtual int64_t read(char * data, uint64_t nbytes, runtime::Time timeout);
+
+      int fd() const { return fd_; }
+
+    private:
+      int fd_;
+      int is_readable_ : 1;
+      int is_writable_ : 1;
+    };
+  }
+}
+
+#endif /* !MIMOSA_STREAM_DIRECT_FD_STREAM_HH */
