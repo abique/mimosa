740c7a84827732e0c34d2e257888e606990b7c12
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-29 14:14:46 +0900

    override is not yet supported.

--- mimosa/stream/fd-stream.hh
+++ mimosa/stream/fd-stream.hh
@@ -11,19 +11,19 @@ namespace mimosa
     {
     public:
       FdStream(int fd);
-      virtual ~FdStream() override;
+      virtual ~FdStream();
 
-      virtual bool atEnd() const override;
+      virtual bool atEnd() const;
 
-      virtual uint64_t bytesAvailable() const override;
-      virtual uint64_t bytesToWrite() const override;
+      virtual uint64_t bytesAvailable() const;
+      virtual uint64_t bytesToWrite() const;
 
-      virtual int64_t write(const char * data, uint64_t nbytes) override;
-      virtual int64_t read(char * data, uint64_t nbytes) override;
-      virtual void flush() override;
+      virtual int64_t write(const char * data, uint64_t nbytes);
+      virtual int64_t read(char * data, uint64_t nbytes);
+      virtual void flush();
 
-      virtual int64_t pos() const override;
-      virtual bool seek(int64_t offset, SeekType whence) override;
+      virtual int64_t pos() const;
+      virtual bool seek(int64_t offset, SeekType whence);
 
     private:
       int fd_;
