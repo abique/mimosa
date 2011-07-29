494b0e1c4557f1098e84de88ef53deb1926e9777
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-29 17:33:58 +0900

    Re-simplified stream

--- mimosa/stream/stream.hh
+++ mimosa/stream/stream.hh
@@ -13,28 +13,13 @@ namespace mimosa
                    private NonCopyable
     {
     public:
-
-      inline Stream(bool is_readable, bool is_writable, bool is_sequential)
-        : is_readable_(is_readable),
-          is_writable_(is_writable),
-          timeout_(0)
-      {
-      }
-
       inline virtual ~Stream() {}
-
-      inline bool isReadable() const { return is_readable_; }
-      inline bool isWritable() const { return is_writable_; }
 
       virtual int64_t write(const char * data, uint64_t nbytes, runtime::Time timeout = 0) = 0;
       virtual int64_t read(char * data, uint64_t nbytes, runtime::Time timeout = 0) = 0;
-      /** flushes the write buffer */
-      virtual bool flush(runtime::Time timeout) = 0;
 
-    private:
-      unsigned is_readable_ : 1;
-      unsigned is_writable_ : 1;
-      unsigned is_sequential_ : 1;
+      /** flushes the write buffer */
+      virtual bool flush(runtime::Time timeout) {}
     };
   }
 }
