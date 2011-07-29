517b3961b7e71ecae8eaf22531ffd9bd2afaf1d7
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-30 04:34:36 +0900

    Starting TlsStream

---
+++ mimosa/stream/tls-stream.hh
@@ -0,0 +1,14 @@
+#ifndef MIMOSA_STREAM_TLS_STREAM_HH
+# define MIMOSA_STREAM_TLS_STREAM_HH
+
+namespace mimosa
+{
+  namespace stream
+  {
+    class TlsStream
+    {
+    };
+  }
+}
+
+#endif /* !MIMOSA_STREAM_TLS_STREAM_HH */
