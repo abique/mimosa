860d25e92bdbd1ad8f7f791e660aee6d426b09d2
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-31 19:41:13 +0900

    Added suport for bison in cmake

---
+++ cmake/Bison.cmake
@@ -0,0 +1,6 @@
+macro(mimosa_bison name)
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${name}.hh ${CMAKE_CURRENT_SOURCE_DIR}/${name}.cc
+    COMMAND bison ${name}.y
+    DEPENDS ${name}.y
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+endmacro(mimosa_bison)
