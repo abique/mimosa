3fdc034d43b60de96e5a6ad0555333dd5a906c5b
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-31 19:41:53 +0900

    Updated build system

--- cmake/Flex.cmake
+++ cmake/Flex.cmake
@@ -3,6 +3,4 @@ macro(mimosa_flex name)
     COMMAND flex ${name}.l
     DEPENDS ${name}.l
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-  # set_source_files_properties(${name}.hh ${name}.cc
-  #   PROPERTIES GENERATED true)
 endmacro(mimosa_flex)
