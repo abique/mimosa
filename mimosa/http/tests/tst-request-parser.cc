e98a4491c968e630053218ae4ab3caab1c3f491a
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-31 21:18:42 +0900

    Moved stuff to a anon namespace

--- mimosa/http/tests/tst-request-parser.cc
+++ mimosa/http/tests/tst-request-parser.cc
@@ -6,22 +6,25 @@ namespace mimosa
 {
   namespace http
   {
+    namespace
+    {
 #define METHOD_TEST(Meth)                               \
-    TEST(RequestParser, Meth##Simple)                   \
-    {                                                   \
-      char str[] = #Meth " / HTTP/1.0\r\n\r\n";         \
-      Request rq;                                       \
-      EXPECT_EQ(1, rq.parse(str, sizeof (str)));        \
-    }
+      TEST(RequestParser, Meth##Simple)                 \
+      {                                                 \
+        char str[] = #Meth " / HTTP/1.0\r\n\r\n";       \
+        Request rq;                                     \
+        EXPECT_EQ(1, rq.parse(str, sizeof (str)));      \
+      }
 
-    METHOD_TEST(GET)
-    METHOD_TEST(HEAD)
-    METHOD_TEST(PUT)
-    METHOD_TEST(DELETE)
-    METHOD_TEST(TRACE)
-    METHOD_TEST(OPTIONS)
-    METHOD_TEST(CONNECT)
-    METHOD_TEST(PATCH)
+      METHOD_TEST(GET)
+      METHOD_TEST(HEAD)
+      METHOD_TEST(PUT)
+      METHOD_TEST(DELETE)
+      METHOD_TEST(TRACE)
+      METHOD_TEST(OPTIONS)
+      METHOD_TEST(CONNECT)
+      METHOD_TEST(PATCH)
+    }
   }
 }
 
