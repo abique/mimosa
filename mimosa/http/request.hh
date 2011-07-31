b4411fb6c5c37de938e5d74fc57f0862dd6c9442
Alexandre Bique <bique.alexandre@gmail.com>
2011-07-31 19:29:57 +0900

    Removed useless fields from Request

--- mimosa/http/request.hh
+++ mimosa/http/request.hh
@@ -5,8 +5,9 @@
 # include <vector>
 # include <unordered>
 
-# include "coding.hh"
 # include "../stream/stream.hh"
+# include "coding.hh"
+# include "method.hh"
 
 namespace mimosa
 {
@@ -15,22 +16,24 @@ namespace mimosa
     class Request
     {
     public:
-      Method                                       method_;
-      std::string                                  raw_location_;
-      uint8_t                                      proto_major_;
-      uint8_t                                      proto_minor_;
-      Coding                                       accept_encoding_;
-      Coding                                       transfert_encoding_;
-      bool                                         keep_alive_;
-      std::vector<Cookie::Ptr>                     cookies_;
-      uint32_t                                     content_length_;
-      std::string                                  content_type_;
-      std::string                                  host_;
-      uint64_t                                     range_start_;
-      uint64_t                                     range_end_;
-      std::string                                  referer_;
-      std::unordered_map<std::string, std::string> unparsed_headers_;
-      stream::Stream::Ptr                          body_;
+      Request();
+
+      Method                                            method_;
+      std::string                                       raw_location_;
+      uint8_t                                           proto_major_;
+      uint8_t                                           proto_minor_;
+      Coding                                            accept_encoding_;
+      bool                                              keep_alive_;
+      std::vector<Cookie::Ptr>                          cookies_;
+      uint64_t                                          content_length_;
+      std::string                                       content_type_;
+      std::string                                       host_;
+      uint64_t                                          range_start_;
+      uint64_t                                          range_end_;
+      std::string                                       referrer_;
+      std::unordered_multimap<std::string, std::string> unparsed_headers_;
+      std::string                                       user_agent_;
+      stream::Stream::Ptr                               body_;
     };
   }
 }
