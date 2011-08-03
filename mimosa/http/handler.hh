527a44e188af88cf3b767a469bc12a5b4e2a1846
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-03 23:45:50 +0200

    Handler now returns true or false

--- mimosa/http/handler.hh
+++ mimosa/http/handler.hh
@@ -14,7 +14,7 @@ namespace mimosa
     {
     public:
       virtual ~Handler() {}
-      virtual void handle(Request & request, Response & response) const = 0;
+      virtual bool handle(Request & request, Response & response) const = 0;
     };
   }
 }
