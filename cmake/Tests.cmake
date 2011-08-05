edbff001bb52fd0659f255476a2aefd9ef4cee23
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-05 16:40:11 +0200

    Improved test suite integration

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -1,4 +1,8 @@
+add_custom_target(tests)
+
 macro(mimosa_test MODULE SOURCES)
   add_executable(tst-${MODULE} EXCLUDE_FROM_ALL ${SOURCES})
   target_link_libraries(tst-${MODULE} ${MODULE} gtest_main gtest pthread)
+  add_custom_target(run-tst-${MODULE} ./tst-${MODULE} DEPENDS tst-${MODULE})
+  add_dependencies(tests run-tst-${MODULE})
 endmacro(mimosa_test)
