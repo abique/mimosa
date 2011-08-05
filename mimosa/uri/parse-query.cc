09a0251801407d6469cffc1d34815b6fb5fa6362
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-05 17:11:03 +0200

    Implemented parse query

---
+++ mimosa/uri/parse-query.cc
@@ -0,0 +1,57 @@
+#include "parse-query.hh"
+
+namespace mimosa
+{
+  namespace uri
+  {
+    static void extractKeyValue(const char *& in,
+                                const char * const end,
+                                std::unordered_multimap<std::string, std::string> * kvs)
+    {
+      const char * key     = in;
+      const char * key_end = in;
+
+      while (true)
+      {
+        if (in == end || *in == ';')
+        {
+          if (key < in)
+            kvs->insert(std::make_pair(std::string(key, in - key),
+                                       std::string()));
+          return;
+        }
+
+        if (*in == '=')
+          key_end = in;
+
+        ++in;
+      }
+
+      const char * value     = in;
+
+      while (true)
+      {
+        if (in == end || *in == ';')
+        {
+          if (key < key_end)
+            kvs->insert(std::make_pair(std::string(key, key_end - key),
+                                       std::string(value, in - value)));
+          return;
+        }
+
+        ++in;
+      }
+    }
+
+    void parseQuery(const char * const                                  input,
+                    uint32_t const                                      len,
+                    std::unordered_multimap<std::string, std::string> * kvs)
+    {
+      const char *       in  = input;
+      const char * const end = input + len;
+
+      while (in < end)
+        extractKeyValue(in, end, kvs);
+    }
+  }
+}
