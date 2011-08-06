789ffabe2da9af4cc998210249fbc465d9e2babe
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-06 02:12:49 +0200

    Fixed mimosa_test macro

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -1,7 +1,7 @@
 add_custom_target(tests)
 
-macro(mimosa_test MODULE SOURCES)
-  add_executable(tst-${MODULE} EXCLUDE_FROM_ALL ${SOURCES})
+macro(mimosa_test MODULE)
+  add_executable(tst-${MODULE} EXCLUDE_FROM_ALL ${ARGN})
   target_link_libraries(tst-${MODULE} ${MODULE} gtest_main gtest pthread)
   add_custom_target(run-tst-${MODULE} ./tst-${MODULE} DEPENDS tst-${MODULE})
   add_dependencies(tests run-tst-${MODULE})
