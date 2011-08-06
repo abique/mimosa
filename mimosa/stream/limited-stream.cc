7c343e03ff094245522e7b38fcfb9df68630ce62
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-06 17:28:27 +0200

    Added LimitedStream

---
+++ mimosa/stream/limited-stream.cc
@@ -0,0 +1,52 @@
+#include <errno.h>
+
+#include "limited-stream.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    LimitedStream::LimitedStream(Stream::Ptr stream)
+      : stream_(stream),
+        rbytes_left_(0),
+        wbytes_left_(0)
+    {
+    }
+
+    int64_t
+    LimitedStream::write(const char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      uint64_t can_write = nbytes > wbytes_left_ ? wbytes_left_ : nbytes;
+      if (can_write == 0)
+      {
+        errno = EFBIG;
+        return -1;
+      }
+
+      int64_t wbytes = stream_->write(data, can_write, timeout);
+      if (wbytes <= 0)
+        return wbytes;
+
+      wbytes_left_ -= wbytes;
+      return wbytes;
+    }
+
+    int64_t
+    LimitedStream::read(char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      uint64_t can_read = nbytes > rbytes_left_ ? rbytes_left_ : nbytes;
+      if (can_read == 0)
+      {
+        errno = EFBIG;
+        return -1;
+      }
+
+      int64_t rbytes = stream_->write(data, can_read, timeout);
+      if (rbytes <= 0)
+        return rbytes;
+
+      rbytes_left_ -= rbytes;
+      return rbytes;
+    }
+  }
+}
---
+++ mimosa/stream/limited-stream.hh
@@ -0,0 +1,29 @@
+#ifndef MIMOSA_STREAM_LIMITED_STREAM_HH
+# define MIMOSA_STREAM_LIMITED_STREAM_HH
+
+# include "stream.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    class LimitedStream : public Stream
+    {
+    public:
+      LimitedStream(Stream::Ptr stream);
+
+      virtual int64_t write(const char * data, uint64_t nbytes, runtime::Time timeout = 0);
+      virtual int64_t read(char * data, uint64_t nbytes, runtime::Time timeout = 0);
+
+      inline void setReadLimit(uint64_t bytes) { rbytes_left_ = bytes; }
+      inline void setWriteLimit(uint64_t bytes) { wbytes_left_ = bytes; }
+
+    private:
+      Stream::Ptr stream_;
+      uint64_t    rbytes_left_;
+      uint64_t    wbytes_left_;
+    };
+  }
+}
+
+#endif /* !MIMOSA_STREAM_LIMITED_STREAM_HH */
