bae6b6733786986bc70883751ddd6528816a2406
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-16 19:33:34 +0200

    Added some tests for ref counting

---
+++ mimosa/tests/tst-ref-countable.cc
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include "../ref-countable.hh"
+
+namespace mimosa
+{
+  namespace
+  {
+    struct A : public RefCountable<A>
+    {
+      int v;
+    };
+
+    TEST(RefCountable, Simple)
+    {
+      A::Ptr obj = new A;
+      obj = 0;
+    }
+
+    TEST(RefCountable, CopyConstructor)
+    {
+      A::Ptr obj = new A;
+      A::Ptr obj2(obj);
+    }
+
+    TEST(RefCountable, Assign)
+    {
+      A::Ptr obj = new A;
+      A::Ptr obj2;
+
+      obj2 = nullptr;
+      obj2 = obj;
+      obj2 = nullptr;
+      obj2 = obj;
+    }
+  }
+}
