cc828553875fa1917a5f83d99469a211e16a68ee
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-17 20:13:25 +0200

    Using MELON_SECONDS etc...

--- mimosa/runtime/time.hh
+++ mimosa/runtime/time.hh
@@ -9,13 +9,13 @@ namespace mimosa
   {
     typedef ::melon_time_t Time;
     inline Time time() { return ::melon_time(); }
-    inline Time nanoseconds(int64_t ns) { return ns; }
-    inline Time microseconds(int64_t us) { return us * 1000; }
-    inline Time milliseconds(int64_t ms) { return ms * 1000 * 1000; }
-    inline Time seconds(int64_t s) { return s * 1000 * 1000 * 1000; }
-    inline Time minutes(int64_t m) { return m * 60 * 1000 * 1000 * 1000; }
-    inline Time hours(int64_t h) { return h * 60 * 60 * 1000 * 1000 * 1000; }
-    inline Time days(int64_t d) { return d * 24 * 60 * 60 * 1000 * 1000 * 1000; }
+    inline Time nanoseconds(int64_t ns) { return ns * MELON_NANOSECOND; }
+    inline Time microseconds(int64_t us) { return us * MELON_MICROSECOND; }
+    inline Time milliseconds(int64_t ms) { return ms * MELON_MILLISECOND; }
+    inline Time seconds(int64_t s) { return s * MELON_SECOND; }
+    inline Time minutes(int64_t m) { return m * MELON_MINUTE; }
+    inline Time hours(int64_t h) { return h * MELON_HOUR; }
+    inline Time days(int64_t d) { return d * MELON_DAY; }
     inline void sleep(Time duration) { ::melon_usleep(duration / 1000); }
   }
 }
