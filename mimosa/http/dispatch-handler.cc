8f4f3c4c83edb402560293fcf4454b35e3a30140
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-19 13:40:45 +0200

    Fixed dispatch handler fnmatch configuration

--- mimosa/http/dispatch-handler.cc
+++ mimosa/http/dispatch-handler.cc
@@ -24,7 +24,7 @@ namespace mimosa
     {
       response.content_type_ = "text/html";
       for (auto it = handlers_.cbegin(); it != handlers_.cend(); ++it)
-        if (!::fnmatch(it->first.c_str(), request.location().c_str(), FNM_PATHNAME))
+        if (!::fnmatch(it->first.c_str(), request.location().c_str(), 0))
           return it->second->handle(request, response);
       return error_handler_->handle(request, response, kStatusNotFound);
     }
