93cd3840e4db586e21497c7207136eddc7073a68
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-19 15:01:08 +0200

    Improved samble web-server

--- mimosa/http/samples/web-server.cc
+++ mimosa/http/samples/web-server.cc
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include <gflags/gflags.h>
 
 #include <mimosa/main.hh>
@@ -15,13 +17,45 @@ class HelloHandler : public mimosa::http::Handler
   virtual bool handle(mimosa::http::RequestReader &  /*request*/,
                       mimosa::http::ResponseWriter & response) const
   {
-    static const std::string hello_world =
+    std::ostringstream os;
+    os <<
       "<html><head><title>Hello World!</title></head>"
-      "<body>Hello World!</body></html>";
+      "<body>Hello World!<hr/>"
+      "<ul>"
+      "<li>browse <a href='data/'>" << FLAGS_path << "</a></li>"
+      "<li>test <a href='query_echo?a=b&c=d'>query string</a></li>"
+      "<li>test <a href='post_echo'>post data</a></li>"
+      "</ul>"
+      "</body></html>";
 
+    std::string data(os.str());
     response.status_ = mimosa::http::kStatusOk;
     response.content_type_ = "text/html";
-    response.write(hello_world.data(), hello_world.size());
+    response.write(data.data(), data.size());
+    return true;
+  }
+};
+
+class QueryEchoHandler : public mimosa::http::Handler
+{
+  virtual bool handle(mimosa::http::RequestReader &  request,
+                      mimosa::http::ResponseWriter & response) const
+  {
+    std::ostringstream os;
+    os <<
+      "<html><head><title>Query Echo!</title></head>"
+      "<body>Query Echo!<hr/>"
+      "<ul>";
+    for (auto it = request.query().cbegin(); it != request.query().cend(); ++it)
+      os << "<li>" << it->first << " = " << it->second << "</li>";
+    os <<
+      "</ul>"
+      "</body></html>";
+
+    std::string data(os.str());
+    response.status_ = mimosa::http::kStatusOk;
+    response.content_type_ = "text/html";
+    response.write(data.data(), data.size());
     return true;
   }
 };
@@ -33,6 +67,7 @@ MIMOSA_MAIN(argc, argv)
 
   auto dispatch(new http::DispatchHandler);
   dispatch->registerHandler("/", new HelloHandler);
+  dispatch->registerHandler("/query_echo", new QueryEchoHandler);
   dispatch->registerHandler("/data/*", new http::FsHandler(FLAGS_path, 1, true));
   http::Server::Ptr server = new http::Server;
   server->setHandler(dispatch);
