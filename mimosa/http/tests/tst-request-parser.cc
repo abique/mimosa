be41deab3f50ad0f21fd720159430f741b56f4f4
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-21 19:44:56 +0200

    Added more tests for the request parser

--- mimosa/http/tests/tst-request-parser.cc
+++ mimosa/http/tests/tst-request-parser.cc
@@ -19,6 +19,7 @@ namespace mimosa
       METHOD_TEST(GET)
       METHOD_TEST(HEAD)
       METHOD_TEST(PUT)
+      METHOD_TEST(POST)
       METHOD_TEST(DELETE)
       METHOD_TEST(TRACE)
       METHOD_TEST(OPTIONS)
@@ -61,6 +62,26 @@ namespace mimosa
         EXPECT_NE(it, rq.unparsedHeaders().end());
         if (it != rq.unparsedHeaders().end())
           EXPECT_EQ(it->second, "SomeValue");
+      }
+
+      TEST(RequestParser, PostHeader)
+      {
+        const char str[] = "POST /post-echo HTTP/1.1\r\n"
+          "Host: localhost:4242\r\n"
+          "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:6.0) Gecko/20100101 Firefox/6.0\r\n"
+          "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+          "Accept-Language: en,fr;q=0.5\r\n"
+          "Accept-Encoding: gzip, deflate\r\n"
+          "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\n"
+          "DNT: 1\r\n"
+          "Connection: keep-alive\r\n"
+          "Referer: http://localhost:4242/post-echo\r\n"
+          "Cache-Control: max-age=0\r\n"
+          "Content-Type: application/x-www-form-urlencoded\r\n"
+          "Content-Length: 29\r\n"
+          "\r\n";
+        Request rq;
+        EXPECT_EQ(true, rq.parse(str, sizeof (str)));
       }
     }
   }
