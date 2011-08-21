96f7d07ed3ce45a80d7d5f8c6a0253bc5efc891b
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-21 19:46:16 +0200

    Improved sample web-server to show form handling

--- mimosa/http/samples/web-server.cc
+++ mimosa/http/samples/web-server.cc
@@ -23,8 +23,8 @@ class HelloHandler : public mimosa::http::Handler
       "<body>Hello World!<hr/>"
       "<ul>"
       "<li>browse <a href='data/'>" << FLAGS_path << "</a></li>"
-      "<li>test <a href='query_echo?a=b&c=d'>query string</a></li>"
-      "<li>test <a href='post_echo'>post data</a></li>"
+      "<li>test <a href='query-echo?a=b&c=d'>query string</a></li>"
+      "<li>test <a href='post-echo'>post data</a></li>"
       "</ul>"
       "</body></html>";
 
@@ -60,6 +60,39 @@ class QueryEchoHandler : public mimosa::http::Handler
   }
 };
 
+class PostEchoHandler : public mimosa::http::Handler
+{
+  virtual bool handle(mimosa::http::RequestReader &  request,
+                      mimosa::http::ResponseWriter & response) const
+  {
+    std::ostringstream os;
+    os <<
+      "<html><head><title>Post Echo!</title></head>"
+      "<body>Post Echo!<hr/>"
+      "Method: " << methodString(request.method()) << "<br/>"
+      "Content-Type: " << request.contentType() << "<br/>"
+      "<ul>";
+    container::kvs form = request.form();
+    for (auto it = form.cbegin(); it != form.cend(); ++it)
+      os << "<li>" << it->first << " = " << it->second << "</li>";
+    os <<
+      "</ul>"
+      "<hr/>"
+      "<form method='post' enctype='application/x-www-form-urlencoded' name='test-form'>"
+      "firstname <input type='text' name='fname'/><br/>"
+      "lastname <input type='text' name='lname'/><br/>"
+      "<input type='submit' value='submit'/>"
+      "</form>"
+      "</body></html>";
+
+    std::string data(os.str());
+    response.status_ = mimosa::http::kStatusOk;
+    response.content_type_ = "text/html";
+    response.write(data.data(), data.size());
+    return true;
+  }
+};
+
 MIMOSA_MAIN(argc, argv)
 {
   (void)argc;
@@ -67,7 +100,8 @@ MIMOSA_MAIN(argc, argv)
 
   auto dispatch(new http::DispatchHandler);
   dispatch->registerHandler("/", new HelloHandler);
-  dispatch->registerHandler("/query_echo", new QueryEchoHandler);
+  dispatch->registerHandler("/query-echo", new QueryEchoHandler);
+  dispatch->registerHandler("/post-echo", new PostEchoHandler);
   dispatch->registerHandler("/data/*", new http::FsHandler(FLAGS_path, 1, true));
   http::Server::Ptr server = new http::Server;
   server->setHandler(dispatch);
