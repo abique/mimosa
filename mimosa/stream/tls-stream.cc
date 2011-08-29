4cb387d97ecaa4c8b7629cdb67da935d7fcf11e9
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-29 19:30:15 +0200

    Finished TlsStream

--- mimosa/stream/tls-stream.cc
+++ mimosa/stream/tls-stream.cc
@@ -1,26 +1,28 @@
+#include <limits>
+
 #include "tls-stream.hh"
 
 namespace mimosa
 {
   namespace stream
   {
-    static ssize_t readWrapper(TlsStream * stream,
-                               char * data,
-                               size_t nbytes)
+    ssize_t
+    TlsStream::readWrapper(TlsStream * stream, char * data, size_t nbytes)
     {
-      return stream->read(data, nbytes);
+      return stream->stream_->read(data, nbytes, stream->read_timeout_);
     }
 
-    static ssize_t writeWrapper(TlsStream * stream,
-                                const char * data,
-                                size_t nbytes)
+    ssize_t
+    TlsStream::writeWrapper(TlsStream * stream, const char * data, size_t nbytes)
     {
-      return stream->write(data, nbytes);
+      return stream->stream_->write(data, nbytes, stream->write_timeout_);
     }
 
     TlsStream::TlsStream(Stream::Ptr stream, bool is_server)
-        : stream_(stream),
-          session_()
+      : stream_(stream),
+        session_(),
+        read_timeout_(0),
+        write_timeout_(0)
     {
       gnutls_init(&session_, is_server ? GNUTLS_SERVER : GNUTLS_CLIENT);
       gnutls_transport_set_ptr(session_, this);
@@ -33,6 +35,22 @@ namespace mimosa
     {
       gnutls_deinit(session_);
       session_ = nullptr;
+    }
+
+    int64_t
+    TlsStream::write(const char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      write_timeout_ = timeout;
+      return gnutls_record_send(session_, data, nbytes > std::numeric_limits<ssize_t>::max() ?
+                                std::numeric_limits<ssize_t>::max() : nbytes);
+    }
+
+    int64_t
+    TlsStream::read(char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      read_timeout_ = timeout;
+      return gnutls_record_recv(session_, data, nbytes > std::numeric_limits<ssize_t>::max() ?
+                                std::numeric_limits<ssize_t>::max() : nbytes);
     }
   }
 }
--- mimosa/stream/tls-stream.hh
+++ mimosa/stream/tls-stream.hh
@@ -15,11 +15,19 @@ namespace mimosa
       TlsStream(Stream::Ptr stream, bool is_server);
       ~TlsStream();
 
+      virtual int64_t write(const char * data, uint64_t nbytes, runtime::Time timeout = 0);
+      virtual int64_t read(char * data, uint64_t nbytes, runtime::Time timeout = 0);
+
       inline gnutls_session_t session() { return session_; }
 
     private:
+      static ssize_t readWrapper(TlsStream * stream, char * data, size_t nbytes);
+      static ssize_t writeWrapper(TlsStream * stream, const char * data, size_t nbytes);
+
       Stream::Ptr      stream_;
       gnutls_session_t session_;
+      runtime::Time    read_timeout_;
+      runtime::Time    write_timeout_;
     };
   }
 }
