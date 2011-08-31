d10a4f877c98f26cc4ac2e3166eb4ed5520fc725
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-31 09:56:53 +0200

    Fixed conditon -> condition!

---
+++ mimosa/sync/condition.hh
@@ -0,0 +1,41 @@
+#ifndef MIMOSA_SYNC_CONDITION_HH
+# define MIMOSA_SYNC_CONDITION_HH
+
+# include <stdexcept>
+
+# include <melon/melon.h>
+
+# include "../non-copyable.hh"
+
+namespace mimosa
+{
+  namespace sync
+  {
+    class Condition : private NonCopyable
+    {
+    public:
+      inline Condition() throw std::bad_alloc
+        : cond_(::melon_cond_new())
+      {
+        if (!cond_)
+          throw std::bad_alloc;
+      }
+
+      inline ~Condition() { ::melon_cond_destroy(cond_); }
+
+      template <typename Mutex>
+      inline void wait(Mutex & mutex) { ::melon_cond_wait(cond_, mutex.mutex_); }
+
+      template <typename Mutex>
+      inline void timedWait(Mutex & mutex, ::melon_time_t time)
+      { ::melon_cond_timedwait(cond_, mutex.mutex_, time); }
+
+      inline void wakeOne() { ::melon_cond_signal(cond_); }
+      inline void wakeAll() { ::melon_cond_broadcast(cond_); }
+    private:
+      ::melon_cond * cond_;
+    };
+  }
+}
+
+#endif /* !MIMOSA_SYNC_CONDITION_HH */
