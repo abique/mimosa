9019cb0e8d64e0eb82410dc50e78a3f70b479b47
Alexandre Bique <bique.alexandre@gmail.com>
2011-09-06 16:39:38 +0200

    Implemented the rpc server sample

--- mimosa/rpc/samples/kv-db.cc
+++ mimosa/rpc/samples/kv-db.cc
@@ -1,7 +1,20 @@
 #include <mimosa/main.hh>
+#include <mimosa/rpc/server.hh>
+
+#include "database.hh"
+
+using namespace mimosa;
 
 MIMOSA_MAIN(argc, argv)
 {
   (void)argc;
   (void)argv;
+
+  rpc::ServiceMap::Ptr service_map = new rpc::ServiceMap;
+  service_map->add(new rpc::samples::Database);
+
+  rpc::Server::Ptr server = new rpc::Server;
+  server->setServiceMap(service_map);
+  server->listenInet4(4242);
+  return 0;
 }
