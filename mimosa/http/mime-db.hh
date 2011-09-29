84de1f4c390042e0bb696a363440c222eb25b879
Alexandre Bique <bique.alexandre@gmail.com>
2011-09-29 20:14:19 +0200

    Starting a mime-database

---
+++ mimosa/http/mime-db.hh
@@ -0,0 +1,16 @@
+#ifndef MIMOSA_HTTP_MIME_DB_HH
+# define MIMOSA_HTTP_MIME_DB_HH
+
+# include "../non-copyable.hh"
+
+namespace mimosa
+{
+  namespace http
+  {
+    class MimeDb : public NonCopyable
+    {
+    };
+  }
+}
+
+#endif /* !MIMOSA_HTTP_MIME_DB_HH */
