ea201a4d6117f1e196923b668645e902eda3722e
Alexandre Bique <bique.alexandre@gmail.com>
2011-10-27 17:10:13 +0200

    Preparing tests for stream namespace

---
+++ mimosa/stream/tests/tst-compress-stream.cc
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include "../compress-stream.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    namespace
+    {
+      TEST(CompressStream, Compress)
+      {
+      }
+
+      TEST(CompressStream, Decompress)
+      {
+      }
+    }
+  }
+}
