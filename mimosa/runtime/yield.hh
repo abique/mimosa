6fd24245f1d1635c6f806e96ff5f6e7b94f3e01d
Alexandre Bique <bique.alexandre@gmail.com>
2011-11-02 11:09:59 +0100

    Using sched_yield instead of melon_yield

--- mimosa/runtime/yield.hh
+++ mimosa/runtime/yield.hh
@@ -1,11 +1,13 @@
 #ifndef MIMOSA_RUNTIME_YIELD_HH
 # define MIMOSA_RUNTIME_YIELD_HH
 
+# include <sched.h>
+
 namespace mimosa
 {
   namespace runtime
   {
-    inline void yield() { ::melon_yield(); }
+    inline void yield() { ::sched_yield(); }
   }
 }
 
