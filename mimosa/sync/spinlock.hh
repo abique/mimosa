3f401d4fabae16065dad0748725afc72c8969f88
Alexandre Bique <bique.alexandre@gmail.com>
2011-11-02 19:38:41 +0100

    Fixed spin lock

--- mimosa/sync/spinlock.hh
+++ mimosa/sync/spinlock.hh
@@ -5,6 +5,7 @@
 
 # include "../non-copyable.hh"
 # include "locker.hh"
+# include "unique-locker.hh"
 
 namespace mimosa
 {
@@ -13,12 +14,14 @@ namespace mimosa
     class SpinLock : private NonCopyable
     {
     public:
-      typedef Locker<SpinLock> Locker;
-      typedef UniqueLocker<SpinLock> UniqueLocker;
+      typedef sync::Locker<SpinLock> Locker;
+      typedef sync::UniqueLocker<SpinLock> UniqueLocker;
+
       inline SpinLock() { ::pthread_spin_init(&spinlock_, PTHREAD_PROCESS_PRIVATE); }
-      inline ~SpinLock() { ::pthread_spin_destroy(spinlock_); }
+      inline ~SpinLock() { ::pthread_spin_destroy(&spinlock_); }
       inline void lock() { ::pthread_spin_lock(&spinlock_); }
       inline void unlock() { ::pthread_spin_unlock(&spinlock_); }
+
     private:
       ::pthread_spinlock_t spinlock_;
     };
