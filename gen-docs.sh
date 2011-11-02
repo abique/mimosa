92237adf3b8de31fd0465d68da54901052c21f47
Alexandre Bique <bique.alexandre@gmail.com>
2011-11-02 19:39:17 +0100

    Better gen-docs

--- gen-docs.sh
+++ gen-docs.sh
@@ -9,17 +9,23 @@ do
   fi
 done
 
-cd doc/html &&
-git checkout $(git log --max-parents=0 | head -n 1 | cut -f 2 -d ' ') &&
-cd - &&
+if test -d doc/html/.git; then
+  cd doc/html &&
+  git checkout $(git log --max-parents=0 | head -n 1 | cut -f 2 -d ' ') &&
+  cd -
+else
+  push=0
+fi &&
+
 cp Doxyfile Doxyfile.tmp &&
 sed -i "s/PROJECT_NUMBER.*/PROJECT_NUMBER=$(<VERSION)/g" Doxyfile.tmp
 doxygen Doxyfile.tmp &&
 rm Doxyfile.tmp &&
 sed -i "s,</body>,<script type=\"text/javascript\">var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try{var pageTracker = _gat._getTracker(\"UA-24670421-1\");pageTracker._trackPageview();} catch(err) {}</script></body>,g" doc/html/*.html &&
-cd - &&
-git add * &&
-git commit -a -m "generated docs" &&
+
 if [[ $push = 1 ]]; then
+  cd doc/html &&
+  git add * &&
+  git commit -a -m "generated docs" &&
   git push -f origin HEAD:gh-pages
 fi
