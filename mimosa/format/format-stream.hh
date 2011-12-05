f8b4e10d5f3ae7eb7ac5653fd088edb6ccfa3a5a
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-05 13:47:41 +0100

    Fixed format-stream

--- mimosa/format/format-stream.hh
+++ mimosa/format/format-stream.hh
@@ -2,7 +2,7 @@
 # define MIMOSA_FORMAT_FORMAT_STREAM_HH
 
 # include "format.hh"
-# include "../stream.hh"
+# include "../stream/stream.hh"
 
 namespace mimosa
 {
@@ -10,11 +10,11 @@ namespace mimosa
   {
     template <typename ... Args>
     inline
-    bool format(stream::Ptr    stream,
-                const char *   fmt,
-                Args ...       args)
+    bool format(stream::Stream::Ptr stream,
+                const char *        fmt,
+                Args ...            args)
     {
-      std::string str(format::str(fmt, args));
+      std::string str(format::str(fmt, args...));
       int64_t bytes = stream->loopWrite(str.data(), str.size());
       return bytes == str.size();
     }
