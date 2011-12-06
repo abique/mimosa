5441f841807e8ab78556a611c3dca37b1eb45266
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-06 23:33:19 +0100

    format-stream: support timeout

--- mimosa/format/format-stream.hh
+++ mimosa/format/format-stream.hh
@@ -11,12 +11,22 @@ namespace mimosa
     template <typename ... Args>
     inline
     bool format(stream::Stream::Ptr stream,
+                runtime::Time       timeout,
                 const char *        fmt,
                 Args ...            args)
     {
       std::string str(format::str(fmt, args...));
-      int64_t bytes = stream->loopWrite(str.data(), str.size());
+      int64_t bytes = stream->loopWrite(str.data(), str.size(), timeout);
       return bytes == str.size();
+    }
+
+    template <typename ... Args>
+    inline
+    bool format(stream::Stream::Ptr stream,
+                const char *        fmt,
+                Args ...            args)
+    {
+      return format(stream, static_cast<runtime::Time>(0), fmt, args...);
     }
   }
 }
