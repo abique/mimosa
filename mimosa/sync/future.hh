6fae402c3d470710c05bb14e7a6955ca086121ad
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-09 16:18:40 +0100

    Added Future

---
+++ mimosa/sync/future.hh
@@ -0,0 +1,34 @@
+#ifndef MIMOSA_SYNC_FUTURE_HH
+# define MIMOSA_SYNC_FUTURE_HH
+
+# include "condition.hh"
+# include "mutex.hh"
+
+namespace mimosa
+{
+  namespace sync
+  {
+    template <typename T>
+    class Future : public RefCountable<Future<T> >
+    {
+    public:
+
+      inline Future(const T & value = T(), bool ready = false);
+
+      inline bool isReady();
+      inline void wait();
+      inline T &  get();
+      inline void set(const T & t);
+
+    private:
+      T         value_;
+      bool      ready_;
+      Mutex     lock_;
+      Condition cond_;
+    };
+  }
+}
+
+# include "future.hxx"
+
+#endif /* !MIMOSA_SYNC_FUTURE_HH */
---
+++ mimosa/sync/future.hxx
@@ -0,0 +1,59 @@
+namespace mimosa
+{
+  namespace sync
+  {
+    template <typename T>
+    inline
+    Future<T>::Future(const T & value, bool ready)
+      : value_(t),
+        ready_(ready),
+        lock_(),
+        cond_()
+    {
+    }
+
+    template <typename T>
+    inline
+    bool
+    Future<T>::isReady()
+    {
+      return ready_;
+    }
+
+    template <typename T>
+    inline
+    void
+    Future<T>::wait()
+    {
+      if (ready_)
+        return;
+
+      Mutex::Locker locker(lock_);
+      if (ready_)
+        return;
+
+      cond_.wait(lock_);
+    }
+
+    template <typename T>
+    inline
+    T &
+    Future<T>::get()
+    {
+      wait();
+      return value_;
+    }
+
+    template <typename T>
+    inline
+    void
+    Future<T>::set(const T & value)
+    {
+      Mutex::Locker locker(lock_);
+      assert(!ready_);
+      value_ = value;
+      ready_ = true;
+      cond_.wakeAll();
+    }
+  }
+}
