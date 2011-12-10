88d97c773c5589fe4b1dd4851afd440af932d33c
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-10 15:49:54 +0100

    Added missing stuff

---
+++ mimosa/container/cache.hxx
@@ -0,0 +1,7 @@
+namespace mimosa
+{
+  namespace container
+  {
+
+  }
+}
---
+++ mimosa/container/tests/tst-cache.cc
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "../cache.hh"
+
+namespace mimosa
+{
+  namespace container
+  {
+    namespace
+    {
+      class MyCache : public Cache<int, int>
+      {
+      protected:
+        virtual void cacheMiss(const int & key)
+        {
+          set(key, key + 42);
+        }
+      };
+
+      TEST(Cache, Simple)
+      {
+        MyCache cache;
+
+        auto val = cache.get(0);
+        ASSERT_EQ(val->get(), 42);
+      }
+    }
+  }
+}
