b9d285f8a36e8bac7c172145889b59fad1653df4
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-11 13:32:42 +0100

    Added html escape filter

---
+++ mimosa/stream/html-escape.cc
@@ -0,0 +1,70 @@
+#include <cassert>
+
+#include "html-escape.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    HtmlEscape::HtmlEscape(Stream::Ptr stream)
+      : Filter(stream)
+    {
+    }
+
+    int64_t
+    HtmlEscape::write(const char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      const char * const start = data;
+      const char * const end   = data + nbytes;
+      const char * p           = data;
+
+      while (data < end)
+      {
+        while (p < end && *p != '<' && *p != '>' &&
+               *p != '&' && *p != '"' && *p != '\'')
+          ++p;
+
+        if (data < p)
+        {
+          auto bytes = stream_->write(data, p - data, timeout);
+          if (bytes < 0)
+            return data == start ? bytes : data - start;
+          if (bytes < p - data)
+            return data - start + bytes;
+          data = p;
+        }
+
+        if (p < end)
+        {
+          const char * replace = nullptr;
+          int          size    = 0;
+
+          switch (*p)
+          {
+          case '<':  replace = "&lt;";   size = 4; break;
+          case '>':  replace = "&gt;";   size = 4; break;
+          case '&':  replace = "&amp;";  size = 5; break;
+          case '\'': replace = "&apos;"; size = 6; break;
+          case '"':  replace = "&quot;"; size = 6; break;
+          default: assert(false); break;
+          }
+
+          auto wrote = stream_->loopWrite(replace, size, timeout);
+          if (wrote < 0)
+            return data - start;
+          if (wrote < size)
+            return -1;
+          ++data;
+          ++p;
+        }
+      }
+      return nbytes;
+    }
+
+    int64_t
+    HtmlEscape::read(char * data, uint64_t nbytes, runtime::Time timeout)
+    {
+      assert(false && "TODO");
+    }
+  }
+}
---
+++ mimosa/stream/html-escape.hh
@@ -0,0 +1,23 @@
+#ifndef MIMOSA_STREAM_HTML_ESCAPE_HH
+# define MIMOSA_STREAM_HTML_ESCAPE_HH
+
+# include "stream.hh"
+
+namespace mimosa
+{
+  namespace stream
+  {
+    class HtmlEscape : public Filter
+    {
+    public:
+      MIMOSA_DEF_PTR(HtmlEscape);
+
+      HtmlEscape(Stream::Ptr stream);
+
+      virtual int64_t write(const char * data, uint64_t nbytes, runtime::Time timeout = 0);
+      virtual int64_t read(char * data, uint64_t nbytes, runtime::Time timeout = 0);
+    };
+  }
+}
+
+#endif /* !MIMOSA_STREAM_HTML_ESCAPE_HH */
