6cb84756b7f759d6ddfa49801057627571cc1300
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-13 19:11:41 +0100

    Cleaned up gen-docs.sh

--- gen-docs.sh
+++ gen-docs.sh
@@ -17,11 +17,8 @@ else
   push=0
 fi &&
 
-cp Doxyfile Doxyfile.tmp &&
-sed -i "s/PROJECT_NUMBER.*/PROJECT_NUMBER=$(<VERSION)/g" Doxyfile.tmp
-doxygen Doxyfile.tmp &&
-rm Doxyfile.tmp &&
-sed -i "s,</body>,<script type=\"text/javascript\">var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try{var pageTracker = _gat._getTracker(\"UA-24670421-1\");pageTracker._trackPageview();} catch(err) {}</script></body>,g" doc/html/*.html &&
+sed -i "s/PROJECT_NUMBER.*/PROJECT_NUMBER=$(<VERSION)/g" Doxyfile
+doxygen Doxyfile
 
 if [[ $push = 1 ]]; then
   cd doc/html &&
