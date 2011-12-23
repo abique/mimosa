1524e1dad7802b574552e32f3df016657e2d0b9b
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-23 15:49:03 +0100

    http: more tests

--- mimosa/http/tests/tst-request-parser.cc
+++ mimosa/http/tests/tst-request-parser.cc
@@ -122,6 +122,38 @@ namespace mimosa
         Request rq;
         ASSERT_EQ(true, rq.parse(str, sizeof (str)));
       }
+
+      TEST(RequestParser, Fail2)
+      {
+        const char str[] =
+          "POST /login HTTP/1.1\r\n"
+          "Host: localhost:19042\r\n"
+          "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1\r\n"
+          "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+          "Accept-Language: en,fr;q=0.5\r\n"
+          "Accept-Encoding: gzip, deflate\r\n"
+          "Accept-Charset: UTF-8,*\r\n"
+          "DNT: 1\r\n"
+          "Connection: keep-alive\r\n"
+          "Referer: https://localhost:19042/login\r\n"
+          "Cookie: login=; auth=\r\n"
+          "Content-Type: application/x-www-form-urlencoded\r\n"
+          "Content-Length: 38\r\n";
+        Request rq;
+        ASSERT_EQ(true, rq.parse(str, sizeof (str)));
+        ASSERT_EQ(38, rq.contentLength());
+      }
+
+      TEST(RequestParser, ContentLength1)
+      {
+        const char str[] =
+          "POST /login HTTP/1.1\r\n"
+          "Cookie: login=; auth=\r\n"
+          "Content-Length: 38\r\n";
+        Request rq;
+        ASSERT_EQ(true, rq.parse(str, sizeof (str)));
+        ASSERT_EQ(38, rq.contentLength());
+      }
     }
   }
 }
