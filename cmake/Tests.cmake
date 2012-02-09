086c701d1a83fe180b0076bdf1b9ee031d0f0ffd
Yann Grandmaitre <yanngrandmaitre@gmail.com>
2012-02-09 10:41:29 +0100

    Add EXCLUDE_FROM_ALL to the CMake macro mimosa_test

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -1,7 +1,7 @@
 add_custom_target(check)
 
 macro(mimosa_test MODULE)
-  add_executable(tst-${MODULE} ${ARGN})
+  add_executable(tst-${MODULE} EXCLUDE_FROM_ALL ${ARGN})
   target_link_libraries(tst-${MODULE} gtest_main gtest mimosa)
   add_custom_target(run-tst-${MODULE} ./tst-${MODULE} DEPENDS tst-${MODULE})
   add_dependencies(check run-tst-${MODULE})
