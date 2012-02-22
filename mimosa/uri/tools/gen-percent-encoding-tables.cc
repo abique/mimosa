fc735a69ed84ce095c09daab54492d49cc51ff3c
Alexandre Bique <abique@freebox.fr>
2012-02-22 12:25:01 +0100

    pct encoding: generating two tables, one wich allow reserved and one which disallow them

--- mimosa/uri/tools/gen-percent-encoding-tables.cc
+++ mimosa/uri/tools/gen-percent-encoding-tables.cc
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-bool isValid(char c)
+bool isValid(char c, bool allow_reserved)
 {
   // always convert '+' to simplify decoding :-)
   return ('A' <= c && c <= 'Z') ||
     ('a' <= c && c <= 'z') ||
     ('0' <= c && c <= '9') ||
-    strchr("!*'();:@&=$,/?#[]-_.~", c);
+    (allow_reserved && strchr("!*'();:@&=$,/?#[]-_.~", c));
 }
 
-int main(void)
+void print(bool allow_reserved,
+           const char *comment)
 {
+  printf("/* %s */\n", comment);
   for (int i = 0; i < 256; ++i)
   {
     if (i % 64 == 0)
       printf("0b");
-    printf("%d", isValid((i & ~63) + (63 - (i & 63))));
+    printf("%d", isValid((i & ~63) + (63 - (i & 63)), allow_reserved));
     if (i % 64 == 63)
       printf("ULL,\n");
   }
+}
+
+int main(void)
+{
+  print(false, "no reserved");
+  print(true, "with reserved");
   return 0;
 }
