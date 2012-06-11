ba3cea003d87f3da257c3534fef0d8339abe4649
Alexandre Bique <bique.alexandre@gmail.com>
2012-06-11 12:16:25 +0200

    StringStream: little helpers

--- mimosa/stream/string-stream.hh
+++ mimosa/stream/string-stream.hh
@@ -20,11 +20,19 @@ namespace mimosa
       virtual int64_t read(char * data, uint64_t nbytes);
 
       inline const std::string & str() const { return str_; }
+      inline std::string && moveStr() { read_pos_ = 0; return std::move(str_); }
       inline void setStr(const std::string & str)
       {
         read_pos_ = 0;
-        str_ = str;
+        str_      = str;
       }
+      inline void setStr(std::string && str)
+      {
+        read_pos_ = 0;
+        str_      = str;
+      }
+
+      inline void clear() { read_pos_ = 0; str_.clear(); }
 
     private:
       std::string::size_type read_pos_;
