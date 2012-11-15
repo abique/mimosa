2ab24c011e802ee25c54006c65f9592a1acb415d
Alexandre Bique <bique.alexandre@gmail.com>
2012-11-15 23:15:07 +0100

    Improved cmake mimosa_protoc macro

--- cmake/ProtocMimosa.cmake
+++ cmake/ProtocMimosa.cmake
@@ -1,10 +1,10 @@
-macro(protoc_mimosa name)
+macro(mimosa_protoc name)
   add_custom_command(OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.h ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.cc
     COMMAND protoc --plugin=${mimosa_BINARY_DIR}/mimosa/rpc/gen/protoc-gen-mimosa --mimosa_out=. ${name}.proto
-    DEPENDS ${name}.proto
+    DEPENDS protoc-gen-mimosa ${name}.proto
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
   set_source_files_properties(
     ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.h
     ${CMAKE_CURRENT_SOURCE_DIR}/${name}.pb.cc
     PROPERTIES GENERATED true)
-endmacro(protoc_mimosa)
+endmacro(mimosa_protoc)
