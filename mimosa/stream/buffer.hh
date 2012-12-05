0cd874873b007472d8a4a5f956bf871ea7d8c16c
Alexandre Bique <bique.alexandre@gmail.com>
2012-12-05 20:47:03 +0100

    stream::Buffer: define an intrusive slist with ref counted pointers

--- mimosa/stream/buffer.hh
+++ mimosa/stream/buffer.hh
@@ -29,13 +29,19 @@ namespace mimosa
       void resize(uint64_t size);
 
     private:
-      IntrusiveSlistHook<Buffer*> next_;
+      union {
+        IntrusiveSlistHook<Buffer*>     next_;
+        IntrusiveSlistHook<Buffer::Ptr> next_ptr_;
+      };
       uint64_t                    size_;
       char *                      data_;
 
     public:
       typedef IntrusiveSlist<Buffer, Buffer *, &Buffer::next_> Slist;
       friend class IntrusiveSlist<Buffer, Buffer *, &Buffer::next_>;
+
+      typedef IntrusiveSlist<Buffer, Buffer::Ptr, &Buffer::next_ptr_> SlistPtr;
+      friend class IntrusiveSlist<Buffer, Buffer::Ptr, &Buffer::next_ptr_>;
     };
   }
 }
