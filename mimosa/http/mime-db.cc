9368b3bd30b1dd865a2738049ea3c164bf66442f
Alexandre Bique <bique.alexandre@gmail.com>
2015-12-07 21:09:06 +0100

    More default mime type

--- mimosa/http/mime-db.cc
+++ mimosa/http/mime-db.cc
@@ -49,23 +49,43 @@ namespace mimosa
 
 #define DEF(Ext, Type) mime_types_[Ext] = Type;
 
-      DEF("bmp", "image/bmp");
+      DEF("aac", "audio/x-aac");
+      DEF("c", "text/x-c");
+      DEF("cc", "text/x-c++src");
+      DEF("cpp", "text/x-c++src");
       DEF("css", "text/css");
+      DEF("css", "text/css");
+      DEF("cxx", "text/x-c++src");
+      DEF("flac", "audio/x-flac");
       DEF("gif", "image/gif");
+      DEF("h", "text/x-c");
+      DEF("hh", "text/x-c++hdr");
+      DEF("hpp", "text/x-c++hdr");
       DEF("htm", "text/html");
       DEF("html", "text/html");
+      DEF("hxx", "text/x-c++hdr");
       DEF("ico", "image/x-icon");
       DEF("jpe", "image/jpeg");
       DEF("jpeg", "image/jpeg");
       DEF("jpg", "image/jpeg");
       DEF("js", "application/javascript");
       DEF("json", "application/json");
+      DEF("mkv", "video/x-matroska");
+      DEF("mp3", "audio/mpeg");
+      DEF("mp4", "video/mp4");
+      DEF("mp4", "video/mpg4");
+      DEF("mp4a", "audio/mp4");
+      DEF("oga", "audio/x-ogg");
+      DEF("ogg", "audio/x-ogg");
+      DEF("ogv", "video/x-ogg");
       DEF("pdf", "application/pdf");
       DEF("png", "image/png");
       DEF("svg", "image/svg+xml");
       DEF("svgz", "image/svg+xml");
       DEF("torrent", "application/x-bittorrent");
+      DEF("text", "text/plain");
       DEF("txt", "text/plain");
+      DEF("wav", "audio/x-wav");
       DEF("xml", "text/xml");
 
       while (std::getline(in, line))
