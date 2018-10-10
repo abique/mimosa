536b71464d213d970185bfe43245a064183e1c96
Alexandre Bique <bique.alexandre@gmail.com>
2018-10-10 02:17:04 +0200

    Fix indentation

--- mimosa/compat/uio.hh
+++ mimosa/compat/uio.hh
@@ -1,14 +1,14 @@
 #pragma once
 
-# include <sys/types.h>
-# include <cstdint>
+#include <sys/types.h>
+#include <cstdint>
 
-# if defined(__unix__) || defined(__APPLE__) || defined(__HAIKU__)
-#  include <sys/uio.h>
-# else
-#  define IOV_MAX 128
+#if defined(__unix__) || defined(__APPLE__) || defined(__HAIKU__)
+# include <sys/uio.h>
+#else
+# define IOV_MAX 128
 struct iovec {
   void     *iov_base = nullptr;
   uint64_t  iov_len  = 0;
 };
-# endif
+#endif
